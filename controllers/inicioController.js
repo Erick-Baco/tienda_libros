3ef2f67da7da12ea689af3d58d26acb996aecf94
Erick-Baco <jesusbaco542@gmail.com>
2025-02-11 21:29:51 -0600

    renderizado de libros al inicio y funcion de busqueda

--- controllers/inicioController.js
+++ controllers/inicioController.js
@@ -1,10 +1,12 @@
-
+import { Libro } from "../model/libros.js";
 // Request -> Peticion Usuario (req)
 // Resource -> Resultado de la Peticion (res)
 
 
 const inicio = async (req, res) => {
-    res.render("inicio");
+    const libros = await Libro.findAll({});
+    console.log(libros);
+    res.render("inicio", { libros });
 };
 
 // Exportaciones de funciones
